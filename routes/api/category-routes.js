0e1db2f0deefa19fe6f1c40e8ba0f5ad6e486a9d
Badr Almadi <badr.almadhi95@gmail.com>
2023-06-14 21:06:19 -0600

    added to the delete method

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -63,14 +63,21 @@ router.put('/:id', async (req, res) => {
 });
 
 router.delete('/:id', async (req, res) => {
-  const deleteCategory = await Category.destroy({
 
-    where: {
-      id: req.params.id,
+  try {
+    const deleteCategory = await Category.destroy({
+      where: {
+        id: req.params.id,
+      }
+    });
+    if (!deleteCategory) {
+      res.status(500).json({ message: 'No delete with this id' });
     }
+    req.status(200).json(deleteCategory);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 
-  });
-  res.json(deleteCategory);
 });
 
 module.exports = router;
